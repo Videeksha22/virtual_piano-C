814fd6d3144cd8fd52797fbe56f280cb2839446e
Videeksha <161011302+Videeksha22@users.noreply.github.com>
2025-08-27 15:31:16 +0530

    Update and rename C to piano.c

---
+++ piano.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+#include <dos.h>
+
+void main() {
+    int i;
+    char ch = 'y', note = 14;
+
+    clrscr();   
+
+    printf("\t\t\tWelcome in Keyboard Piano\n");
+    printf("Please wait... loading\n");
+    printf("Press X to exit!!\n");
+
+    for (i = 1; i <= 20; i++) {
+        printf("%c", note);
+        delay(100);
+    }
+
+    while (ch != 'X' && ch != 'x') {   
+        ch = getch();                 
+        if (ch != 'X' && ch != 'x') {
+            sound(ch * 10);            
+            delay(200);              
+            nosound();                
+        }
+    }
+}
